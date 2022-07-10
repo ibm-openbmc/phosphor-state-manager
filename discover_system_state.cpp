a6a728b80501cd821e20e3731bc28ec017804e78
Andrew Geissler <geissonator@users.noreply.github.com>
2022-07-09 19:58:56 -0500

    delay apr power on by 30 seconds (#19)
    * delay apr power on by 30 seconds
    
    Recently code was added to move the vpd collection services to later in
    the BMC boot. This caused a conflict with the APR (Automatic Power
    Restart) service. The APR service would start the power on of the host
    before vpd collection had completed. This caused conflicts on the shared
    i2c busses between the BMC and host firmware.
    
    Long term, we need a way to know when the vpd collection services have
    completed and APR should not run until the BMC has reached READY state
    and the vpd collection has completed.
    
    Short term, add a 30 second delay when APR detects it needs to power on
    the system. This ensure APR does not impact the BMC boot when it's not
    needed and adds a safe delay when it does need to boot the system.
    
    Testing showed that we also need the 30s delay, even when just
    requesting the power off, due to it running the same services that
    conflict with vpd collection
    
    Tested:
    - Powered on p10bmc chassis, caused an AC cycle, and verified that APR
      did not initiate the boot until after vpd collection had completed.
      No host i2c errors were logged and system booted as expected.
    - Issued a r/r and verified service did not run
    - Verified service exited quickly when no host power on was needed
    - George and Santosh verified all APR settings and a large config system
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>

--- discover_system_state.cpp
+++ discover_system_state.cpp
@@ -20,6 +20,7 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <thread>
 
 namespace phosphor
 {
@@ -144,7 +145,8 @@ int main(int argc, char** argv)
         if (RestorePolicy::Policy::AlwaysOn ==
             RestorePolicy::convertPolicyFromString(powerPolicy))
         {
-            info("power_policy=ALWAYS_POWER_ON, powering host on");
+            info("power_policy=ALWAYS_POWER_ON, powering host on (30s delay)");
+            std::this_thread::sleep_for(std::chrono::seconds(30));
             phosphor::state::manager::utils::setProperty(
                 bus, hostPath, HOST_BUSNAME, "RestartCause",
                 convertForMessage(
@@ -167,13 +169,15 @@ int main(int argc, char** argv)
         else if (RestorePolicy::Policy::AlwaysOff ==
                  RestorePolicy::convertPolicyFromString(powerPolicy))
         {
-            info("power_policy=ALWAYS_POWER_OFF, set requested state to off");
+            info(
+                "power_policy=ALWAYS_POWER_OFF, set requested state to off (30s delay)");
             // Read last requested state and re-request it to execute it
             auto hostReqState = phosphor::state::manager::utils::getProperty(
                 bus, hostPath, HOST_BUSNAME, "RequestedHostTransition");
             if (hostReqState !=
                 convertForMessage(server::Host::Transition::Off))
             {
+                std::this_thread::sleep_for(std::chrono::seconds(30));
                 phosphor::state::manager::utils::setProperty(
                     bus, hostPath, HOST_BUSNAME, "RequestedHostTransition",
                     convertForMessage(server::Host::Transition::Off));
@@ -182,7 +186,7 @@ int main(int argc, char** argv)
         else if (RestorePolicy::Policy::Restore ==
                  RestorePolicy::convertPolicyFromString(powerPolicy))
         {
-            info("power_policy=RESTORE, restoring last state");
+            info("power_policy=RESTORE, restoring last state (30s delay)");
             // Read last requested state and re-request it to execute it
             auto hostReqState = phosphor::state::manager::utils::getProperty(
                 bus, hostPath, HOST_BUSNAME, "RequestedHostTransition");
@@ -191,6 +195,7 @@ int main(int argc, char** argv)
             if (hostReqState !=
                 convertForMessage(server::Host::Transition::Off))
             {
+                std::this_thread::sleep_for(std::chrono::seconds(30));
                 phosphor::state::manager::utils::setProperty(
                     bus, hostPath, HOST_BUSNAME, "RestartCause",
                     convertForMessage(
